a599daa79ce12ed90b91058c5208f90acf3127da
cyrilondon <15067007+cyrilondon@users.noreply.github.com>
2020-03-29 11:01:59 +0200

    Coordinates object + test

---
+++ src/main/java/com/game/domain/model/entity/Coordinates.java
@@ -0,0 +1,53 @@
+package com.game.domain.model.entity;
+
+import java.util.Objects;
+
+public class Coordinates {
+
+	private int absciss, ordinate;
+
+	public Coordinates(int x, int y) {
+		this.absciss = x;
+		this.ordinate = y;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		
+		if (obj == this) {
+			return true;
+		}
+		
+		if (obj instanceof Coordinates) {
+			Coordinates other = (Coordinates)obj;
+			return Objects.equals(absciss, other.getAbsciss()) && Objects.equals(ordinate, other.getOrdinate());
+		}
+		
+		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(getAbsciss(), getOrdinate());
+	}
+	
+	public int[] getCoordinates() {
+		return new int[] {getAbsciss(), getOrdinate()};
+	}
+	
+	
+
+	public int getAbsciss() {
+		return absciss;
+	}
+
+	public int getOrdinate() {
+		return ordinate;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Coordinates [absciss = %s, ordinate =%s]", getAbsciss(), getOrdinate());
+	}
+
+}
---
+++ src/test/java/com/game/domain/model/entity/CoordinatesTest.java
@@ -0,0 +1,36 @@
+package com.game.domain.model.entity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.testng.annotations.Test;
+
+public class CoordinatesTest {
+	
+	@Test
+	public void testCoordinatesConstructor() {
+		Coordinates coordinates = new Coordinates(3, 4);
+		assertThat(coordinates.getAbsciss()).isEqualTo(3);
+		assertThat(coordinates.getOrdinate()).isEqualTo(4);
+	}
+	
+	@Test
+	public void testCoordinatesEquals() {
+		Coordinates coordinates = new Coordinates(3, 4);
+		Coordinates otherCoordinates = new Coordinates(3, 4);
+		assertThat(coordinates).isEqualTo(otherCoordinates);
+	}
+	
+	@Test
+	public void testCoordinatesHashCode() {
+		Coordinates coordinates = new Coordinates(3, 4);
+		Coordinates otherCoordinates = new Coordinates(3, 4);
+		assertThat(coordinates.hashCode()).isEqualTo(otherCoordinates.hashCode());
+	}
+	
+	@Test
+	public void testToString() {
+		Coordinates coordinates = new Coordinates(3, 4);
+		assertThat(coordinates.toString()).isEqualTo("Coordinates [absciss = 3, ordinate =4]");
+	}
+
+}
